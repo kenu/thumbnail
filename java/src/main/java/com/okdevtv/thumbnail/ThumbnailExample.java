1a4536abcf5f83e2c8813b57d77f99eb228ce7f2
kenu <kenu.heo@gmail.com>
2023-10-31 22:23:12 +0900

    refactor thumbnail

--- java/src/main/java/com/okdevtv/thumbnail/ThumbnailExample.java
+++ java/src/main/java/com/okdevtv/thumbnail/ThumbnailExample.java
@@ -7,14 +7,19 @@ import java.io.IOException;
 
 public class ThumbnailExample {
     public static void main(String[] args) {
+        String input = "images/dog.png";
+        String output = "images/output/dog.jthumb.jpg";
+        getThumbnail(input, output);
+    }
+
+    public static void getThumbnail(String input, String output) {
         try {
             // 원본 이미지 파일
-            File originalImage = new File("images/dog.png");
+            File originalImage = new File(input);
 
             // 새로운 이미지 파일로 저장할 경로와 파일 이름
-            File resizedImage = new File("images/output/dog.jthumb.jpg");
+            File resizedImage = new File(output);
 
-            // 이미지 리사이즈 (가로 200px, 세로 200px)
             Thumbnails.of(originalImage)
                 .size(100, 100)
                 .toFile(resizedImage);
